8fac713a12687b25d8c8063c8adc17d2ede7ac20
ujin2021 <ujin2021@naver.com>
2021-08-24 17:38:10 +0900

    logout 추가

--- jwt-demo/src/main/java/com/example/jwtdemo/controller/AuthController.java
+++ jwt-demo/src/main/java/com/example/jwtdemo/controller/AuthController.java
@@ -1,21 +1,13 @@
 package com.example.jwtdemo.controller;
 
 import com.example.jwtdemo.dto.request.LoginDto;
+import com.example.jwtdemo.dto.request.TokenRequestDto;
 import com.example.jwtdemo.dto.response.TokenDto;
-import com.example.jwtdemo.jwt.JwtFilter;
-import com.example.jwtdemo.jwt.TokenProvider;
+import com.example.jwtdemo.service.AuthService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -23,32 +15,26 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api")
 public class AuthController {
-    private final TokenProvider tokenProvider;
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final AuthService authService;
 
-    public AuthController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
-        this.tokenProvider = tokenProvider;
-        this.authenticationManagerBuilder = authenticationManagerBuilder;
+    public AuthController(AuthService authService) {
+        this.authService = authService;
     }
 
-    @PostMapping("/authenticate")
-    public ResponseEntity<TokenDto> authorize(@Valid @RequestBody LoginDto loginDto) { // loginDto로 post 요청으로 들어온 속성을 받고
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@Valid @RequestBody LoginDto loginDto) { // loginDto로 post 요청으로 들어온 속성을 받고
+        log.info("userId = {}, password = {}", loginDto.getUserId(), loginDto.getPassword());
+        return ResponseEntity.ok(authService.login(loginDto));
+    }
 
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(loginDto.getUserId(), loginDto.getPassword()); // authenticationToken을 생성한다
+    @PostMapping("/reissue")
+    public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
+        return ResponseEntity.ok(authService.reissue(tokenRequestDto));
+    }
 
-        // 위에서 만든 authenticationToken을 받아 authenticate가 실행이 될 때 -> loadByUsername method가 실행된다 !!!
-        // 그 결과값을 가지고 authentication 객체를 생성한다
-        // 그 객체를 securitycontextholder에 저장한다
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        // authentication 객체를 createToken method를 통해 jwt를 생성한다
-        String jwt = tokenProvider.createToken(authentication);
-
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt); // response header에 jwt를 넣어준다
-
-        return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK); // TokenDto를 이용해 body에도 넣어서 리턴
+    @GetMapping("/logout")
+    public ResponseEntity logout() {
+        // refresh token 삭제
+        return new ResponseEntity(HttpStatus.OK); // status code만 return
     }
 }
